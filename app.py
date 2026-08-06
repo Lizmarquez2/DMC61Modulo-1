67376e866edd640293db694c8b86cac69b166faa
Lizmarquez2 <esthef356@outlook.com>
2026-08-05 22:01:49 -0500

    Add module selection and welcome messages

--- app.py
+++ app.py
@@ -11,3 +11,10 @@ lista_numeros = list(range(int(valor_inicial), int(valor_final)))
 st.write(lista_numeros) 
 
 modulos = st.sidebar.selectbox ("Seleccione un módulo",["Módulo Listas","Módulo Arreglos","Módulo Funciones"])
+
+if modulos == "Módulos Listas":
+  st.write("Bienvenido al módulo Listas")
+elif modulos =="Módulos Arreglos":
+  st.write("Bienvenido al módulo Arreglos")
+else:
+  st.write("Bienvenido al módulo Funciones")
