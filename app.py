63746200a4229d6bf8033474a39589a81141c4f6
Lizmarquez2 <esthef356@outlook.com>
2026-08-10 22:07:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ st.title("Especialización Pyhton for Analytics")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Liz Marquez")
 
+st.image ("PYTHON.png",width = 300)
+st.slidebar.image ("DMC.png", width = 100)
+
+
 modulos = st.sidebar.selectbox ("Seleccione un módulo",["Módulo Listas","Módulo Arreglos","Módulo Funciones"])
 
 if modulos == "Módulo Listas":
