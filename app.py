8116d41e2f63f4f4788c725f984926ac209d3c96
Lizmarquez2 <esthef356@outlook.com>
2026-08-12 22:24:49 -0500

    Correct button check from 'buttom' to 'button'
    Fix typo in button check for calculation.

--- app.py
+++ app.py
@@ -39,6 +39,5 @@ else:
   tasa_porcentaje = st.number_input("Tasa de interés anual (%)", min_value=0.0, value=0.05)
 
 
-if st.buttom("Calcular"):
    resultado_interes_simple = lf.interes_simple(capital_inicial,tiempo_meses,tasa_porcentaje)
    st.write(resultado_interes_simple)
